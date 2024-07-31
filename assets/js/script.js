cad56af7bd79ea23d36b7f737b2f49e62a111533
Vitor Gabriel <167909796+VitorGabrielLisboa@users.noreply.github.com>
2024-07-31 10:15:57 -0300

    Alterações no caminho

--- assets/js/script.js
+++ assets/js/script.js
@@ -28,7 +28,7 @@ readMore.onclick = () =>{
 
 button.onclick = () => {
     if(input.value.trim()){
-        window.location.href = 'trilha-css-desafio-02/wrong.html'
+        window.location.href = '../../wrong.html'
     }
 }
     
